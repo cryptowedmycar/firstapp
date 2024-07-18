874e9393f78f73a8c805a52b2538e31dd80653af
cryptowedmycar <wspickerman@gmail.com>
2024-07-18 17:12:31 -0400

    //leetcode 252 meeting rooms

--- src/github.com/cryptowedmycar/firstapp/Main.go
+++ src/github.com/cryptowedmycar/firstapp/Main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+func canAttendMeetings(intervals [][]int) bool {
+	var canAttend bool
+
+	earlyStart, lateEnd := 0, -1
+
+	for i := 0; i < len(intervals); i++ {
+
+		if intervals[i][0] <= earlyStart {
+			earlyStart = intervals[i][0]
+		}
+
+		if intervals[i][1] > lateEnd {
+			lateEnd = intervals[i][1]
+		}
+
+	}
+
+	fmt.Println(earlyStart)
+	fmt.Println(lateEnd)
+
+	//fmt.Println(canAttend)
+	return canAttend
+}
+
 func main() {
-	fmt.Println("Hello Will!")
+	var intervals = [][]int{{0, 30}, {5, 10}, {15, 20}}
+
+	canAttendMeetings(intervals)
+
+	//fmt.Println(intervals)
+
 }
